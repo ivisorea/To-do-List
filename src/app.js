3ff6e3b18ef63a3a79a67d30586515f7eca5e3ce
Ivis Orea <ivisorea@gmail.com>
2021-10-13 12:16:44 +0200

    add text to the input

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ addButton.addEventListener('click', () => {
     // hide empty message
     document.getElementById('empty-list-msg').hidden = true;
     let node = document.createElement('div');
+    let itemText = document.querySelector('.top-shop-item').value;
     node.innerHTML = `
         <div class="input-group">
             <div class="input-group-text">
                 <input class="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input">
             </div>
-            <input type="text" class="form-control" aria-label="Text input with radio button">
+            <input type="text" class="form-control" aria-label="Text input with radio button" value="${itemText}">
             <button class="btn btn-outline-secondary" type="button" id="button-add">
                 <i class="far fa-edit"></i>
             </button>
