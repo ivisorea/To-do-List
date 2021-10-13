bf16a1aa41a91cdc3f3a9ee37b3e2321711c1935
Ivis Orea <ivisorea@gmail.com>
2021-10-13 12:04:41 +0200

    add input

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const addButton = document.getElementById('button-add');
+
+addButton.addEventListener('click', () => {
+    // hide empty message
+    document.getElementById('empty-list-msg').hidden = true;
+    let node = document.createElement('div');
+    node.innerHTML = `
+        <div class="input-group">
+            <div class="input-group-text">
+                <input class="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input">
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with radio button">
+            <button class="btn btn-outline-secondary" type="button" id="button-add">
+                <i class="far fa-edit"></i>
+            </button>
+            <button class="btn btn-outline-secondary" type="button" id="button-add">
+                <i class="far fa-trash-alt" style="color:red;"></i>
+            </button>
+        </div>`
+    document.querySelector('.shopping-list').appendChild(node);
+   
+
+    
+
+});
+
